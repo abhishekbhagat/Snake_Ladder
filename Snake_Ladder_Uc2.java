974cf0c05e5f1018540195ba750b88b34e8fa7fa
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-09-25 22:32:33 +0530

    use random to get number between 1 to 6

---
+++ Snake_Ladder_Uc2.java
@@ -0,0 +1,13 @@
+package abc;
+
+public class Snake_Ladder_Uc2
+{
+	public static void main(String[] args)
+	{
+		int startPosition=0;
+		System.out.println("Starting position : "+startPosition);
+	    int DieRolls=(int)Math.floor((Math.random()*6 +1));
+	    System.out.println("You get : "+DieRolls);
+
+	}
+}
